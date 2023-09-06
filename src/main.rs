31147fd033b8ef62bfbf8cdc34c5e5ad5ab4ebb8
AbrahimZaman360 <abrahimzaman3@gmail.com>
2023-09-06 21:14:52 +0500

    Added Some Learning Curves

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
 fn main() {
     let app_version = "1.0.0";
+    let mut dark = false;
     println!("Dustbin v{app_version}");
+    print!("Dark is {dark}");
+    dark = true;
+    print!("Dark is {dark}");
 }
