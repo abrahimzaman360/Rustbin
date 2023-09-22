540fec29d112e67b1961f6b990393ecaf6a32e0b
AbrahimZaman360 <AbrahimZaman3@gmail.com>
2023-09-22 09:26:40 +0500

    start caching

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,23 @@
+use std::process::exit;
+
+mod functions;
 mod guess;
 mod tictactoe;
 
 fn main() {
-    // init guess game...
-    guess::game::init();
-    tictactoe::tictactoe::init();
+    println!("Which Program You Want to Run");
+    let mut inp = String::new();
+    std::io::stdin()
+        .read_line(&mut inp)
+        .expect("Error 10k!");
+    if inp.contains("1") {
+        guess::game::init();
+    }
+    else if inp.contains("2") {
+        tictactoe::tictactoe::init();
+    }
+    else if inp.contains("3") {
+        functions::functions::init();
+    }
+    exit(200);
 }
